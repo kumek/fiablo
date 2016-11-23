5830061f2594ddbf7d9af1fe6c5297d31919e45e
Adrian Kuma <adrian.kuma@vml.com>
2016-11-23 13:17:52 +0100

    Added gulp watch for images directory

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,6 +7,7 @@ gulp.task('watch', () => {
 	let watchers = []; 
 	gulp.watch(path.style.src, ['styles']);
 	gulp.watch(path.scripts.src, ['scripts']);
+	gulp.watch(path.images.src, ['images']);
 
 	// watchers.forEach(watcher => watcher.on('change', event => {
 	// 	console.log(`${event.path}`.green + ' - ' + (event.type === 'deleted' ? `${event.type}`.red : `${event.type}`.yellow));
